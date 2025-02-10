c56567d7505aa91caf4167bb97edaf9ed2f8d3a0
saikumar <venkatasaikumar803@gmail.com>
2025-02-10 16:50:43 -0600

    initial commit

---
+++ src/main/java/com/example/SpringBootOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOAuth2Application.class, args);
+	}
+
+}
